e192ad6971b74585e4b54c7084da91bf4e687dd7
Andrei Fedyashev <fedjashev@gmail.com>
2016-12-27 09:44:20 +0200

    Initial commit

---
+++ javascripts/app.js
@@ -0,0 +1,40 @@
+var setUrl = function (word) {
+  return "http://api.flickr.com/services/feeds/photos_public.gne?" +
+    "tags=" + word + "&format=json&jsoncallback=?"
+};
+
+var clearContent = function () {
+  $(".content").empty();
+};
+
+var main = function () {
+  console.log("Hello, World!");
+  //  var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +
+  //   "tags=woman&format=json&jsoncallback=?";
+  // $.getJSON(url, function(flickrResponse) {
+  //   var $list = $(".content");
+  //   flickrResponse.items.forEach(function (photo) {
+  //     var $img = $("<img src=\"" + photo.media.m + "\">");
+  //     $list.append($img);
+  //   });
+  // });
+
+  $("input").on("keypress", function (event) {
+    if (event.keyCode === 13) {
+      var $text = $("input").val();
+      if ($text !== "") {
+        clearContent();
+          $.getJSON(setUrl($text), function(flickrResponse) {
+            var $list = $(".content");
+            flickrResponse.items.forEach(function (photo) {
+              var $img = $("<img src=\"" + photo.media.m + "\">");
+              $list.append($img);
+            });
+          });
+        $("input").val("");
+      }
+    }
+  });
+};
+
+$(document).ready(main);
